5bacae5a56c556f8c2523f39ece795f01a76ef76
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-05 17:04:28 -0500

    First pass on structure.

---
+++ ng-resume/src/app/resume/resume.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'component-resume',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './resume.component.html',
+  styleUrls: ['./resume.component.scss']
+})
+export class ResumeComponent {
+
+}
