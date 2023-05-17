9ad1016868ff6993b73ae17a32a720ef524653b0
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-17 11:02:23 -0500

    Doc and implement more efficient change detection.

--- ng-resume/src/app/resume/components/work-history-item/work-history-item.component.ts
+++ ng-resume/src/app/resume/components/work-history-item/work-history-item.component.ts
@@ -1,7 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkHistory } from '../../models/workhistory';
 
+/**
+ * Component to render elements in the resume work history section.
+ */
 @Component({
   selector: 'app-component-work-history-item',
   standalone: true,
@@ -9,7 +12,8 @@ import { WorkHistory } from '../../models/workhistory';
     CommonModule,
   ],
   templateUrl: './work-history-item.component.html',
-  styleUrls: ['./work-history-item.component.scss']
+  styleUrls: ['./work-history-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WorkHistoryItemComponent {
   @Input() workHistory!: WorkHistory;
