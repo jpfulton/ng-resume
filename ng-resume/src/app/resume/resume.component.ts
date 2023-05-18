c89161c3a0d588eea4e5b3bf18b01ab15e51641d
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-18 11:15:08 -0500

    Clean up with a modern approach to DI.

--- ng-resume/src/app/resume/resume.component.ts
+++ ng-resume/src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkHistoryItemComponent } from './components/work-history-item/work-history-item.component';
 import { WorkHistory } from './models/workhistory';
@@ -6,7 +6,6 @@ import { WorkHistoryService } from './services/workhistory.service';
 import { Education } from './models/education';
 import { EducationService } from './services/education.service';
 import { EducationItemComponent } from './components/education-item/education-item.component';
-import { Subscription } from 'rxjs';
 
 /**
  * Top level component for the resume view heirarchy.
@@ -20,29 +19,19 @@ import { Subscription } from 'rxjs';
     WorkHistoryItemComponent,
   ],
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.scss']
+  styleUrls: ['./resume.component.scss'],
+  providers: [
+    EducationService,
+    WorkHistoryService
+  ]
 })
-export class ResumeComponent implements OnInit, OnDestroy {
+export class ResumeComponent {
   educationList: Education[] = [];
-  educationSubscription: Subscription | undefined;
-  educationService: EducationService = inject(EducationService);
-
   workHistoryList: WorkHistory[] = [];
-  workHistorySubscription: Subscription | undefined;
-  workHistoryService: WorkHistoryService = inject(WorkHistoryService);
 
-  ngOnInit() : void {
-    this.workHistorySubscription =
-      this.workHistoryService.getAllWorkHistoryItems()
-        .subscribe(data => this.workHistoryList = data);
-
-    this.educationSubscription =
-      this.educationService.getAllEducationItems()
-        .subscribe(data => this.educationList = data);
-  }
-
-  ngOnDestroy() : void {
-    this.workHistorySubscription?.unsubscribe();
-    this.educationSubscription?.unsubscribe();
+  constructor(educationService: EducationService, workHistoryService: WorkHistoryService)
+  {
+    educationService.getAllEducationItems().subscribe(data => this.educationList = data);
+    workHistoryService.getAllWorkHistoryItems().subscribe(data => this.workHistoryList = data);
   }
 }
