beb4b5efc10ff6f8fd9abeca0f818c0fdc39f5ea
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-22 10:37:07 -0500

    Introduce not found behavior and component to router.

--- ng-resume/src/app/routes.ts
+++ ng-resume/src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 
 import { ResumeComponent } from './resume/resume.component';
 import { ErrorComponent } from './core/components/error/error.component';
+import { NotFoundComponent } from './core/components/not-found/not-found.component';
 
 const routeConfig: Routes = [
     {
         path: '',
+        pathMatch: 'full',
         component: ResumeComponent,
         title: 'Resume'
     },
@@ -13,6 +15,11 @@ const routeConfig: Routes = [
         path: 'error',
         component: ErrorComponent,
         title: 'Error'
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+        title: 'Not Found'
     }
 ];
 
