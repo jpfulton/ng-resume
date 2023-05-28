1910cc82137a81bbbd573da12ebdd885eb84ba62
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-28 15:34:31 -0500

    Move constructor logic to OnInit and OnDestroy.

--- ng-resume/src/app/resume/resume.component.ts
+++ ng-resume/src/app/resume/resume.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { WorkHistoryItemComponent } from './components/work-history-item/work-history-item.component';
 import { WorkHistory } from './models/workhistory';
 import { WorkHistoryService } from './services/workhistory.service';
+
 import { Education } from './models/education';
 import { EducationService } from './services/education.service';
 import { EducationItemComponent } from './components/education-item/education-item.component';
@@ -25,13 +29,27 @@ import { EducationItemComponent } from './components/education-item/education-it
     WorkHistoryService
   ]
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit, OnDestroy {
   educationList: Education[] = [];
   workHistoryList: WorkHistory[] = [];
 
-  constructor(educationService: EducationService, workHistoryService: WorkHistoryService)
+  private educationSubscription: Subscription | null = null;
+  private workHistorySubscription: Subscription | null = null;
+
+  constructor(
+    private educationService: EducationService, 
+    private workHistoryService: WorkHistoryService
+    )
   {
-    educationService.getAllEducationItems().subscribe(data => this.educationList = data);
-    workHistoryService.getAllWorkHistoryItems().subscribe(data => this.workHistoryList = data);
+  }
+
+  ngOnInit(): void {
+    this.educationSubscription = this.educationService.getAllEducationItems().subscribe(data => this.educationList = data);
+    this.workHistorySubscription = this.workHistoryService.getAllWorkHistoryItems().subscribe(data => this.workHistoryList = data);
+  }
+
+  ngOnDestroy(): void {
+    this.educationSubscription?.unsubscribe();
+    this.workHistorySubscription?.unsubscribe();
   }
 }
