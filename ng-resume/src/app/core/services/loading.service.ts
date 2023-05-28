3ddcb8557e887a1c2298a6b4d923472514296107
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-28 16:58:52 -0500

    Add simple state management for loading state.

---
+++ ng-resume/src/app/core/services/loading.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingService {
+  private loading = false;
+
+  setLoading(loading: boolean) {
+    this.loading = loading;
+  }
+
+  getLoading(): boolean {
+    return this.loading;
+  }
+}
