9a41c11860201d25b90a53b816d83568d5a25e59
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-31 18:46:19 -0500

    Add GitHub link to header.

--- ng-resume/src/app/core/components/header/header.component.ts
+++ ng-resume/src/app/core/components/header/header.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule
+  ]
 })
 export class HeaderComponent {
-
 }
