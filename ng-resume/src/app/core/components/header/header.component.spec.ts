6eb496307218067a86a9e51e82f948b0f13eb29c
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-31 21:49:58 -0500

    Configure test bed module for new header component functionality.

--- ng-resume/src/app/core/components/header/header.component.spec.ts
+++ ng-resume/src/app/core/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterModule, provideRouter } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
+import routeConfig from 'src/app/routes';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +10,13 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HeaderComponent]
+      imports: [
+        RouterModule,
+        HeaderComponent
+      ],
+      providers: [
+        provideRouter(routeConfig),
+      ]
     });
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
