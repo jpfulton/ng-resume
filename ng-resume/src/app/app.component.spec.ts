e02135caafb78f2b9a79117fc8b943f5a0236ad8
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-05-31 21:52:00 -0500

    Configure test bed module to import header and footer components.

--- ng-resume/src/app/app.component.spec.ts
+++ ng-resume/src/app/app.component.spec.ts
@@ -6,6 +6,8 @@ import { NgcCookieConsentModule } from 'ngx-cookieconsent';
 import { AppComponent } from './app.component';
 import { COOKIE_CONSENT_CONFIG } from './core/constants/cookieconsent-constants';
 import { SpinnerComponent } from './core/components/spinner/spinner.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -15,7 +17,9 @@ describe('AppComponent', () => {
     ],
     imports: [
       RouterTestingModule,
-      NgcCookieConsentModule.forRoot(COOKIE_CONSENT_CONFIG)
+      NgcCookieConsentModule.forRoot(COOKIE_CONSENT_CONFIG),
+      HeaderComponent,
+      FooterComponent
     ]
   }));
 
