9c0d4f354f1f847c85f77410843b22754f8f9b73
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-01 17:33:31 -0500

    Expand to include id property.

--- ng-resume/src/app/resume/models/workhistory.ts
+++ ng-resume/src/app/resume/models/workhistory.ts
@@ -2,6 +2,7 @@
  * Interface to define structure of a work history item object.
  */
 export interface WorkHistory {
+    id: string;
     startYear: number;
     endYear: number;
     title: string;
