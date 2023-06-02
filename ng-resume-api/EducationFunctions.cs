3788c20b80c460aaf2c306800cc22e6f20c67fb2
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-01 21:49:42 -0500

    Embed static json in api assembly.

---
+++ ng-resume-api/EducationFunctions.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+using Jpf.NgResume.Api.DataStore;
+using Jpf.NgResume.Api.Models;
+
+namespace Jpf.NgResume.Api
+{
+    public static class EducationFunctions
+    {
+        private static readonly EducationDataStore educationDataStore;
+
+        static EducationFunctions()
+        {
+            educationDataStore = new EducationDataStore();
+        }
+
+        [FunctionName("GetAllEducation")]
+        public static async Task<IActionResult> GetAllEducation(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous, 
+                "get",
+                Route = "education"
+                )
+            ] HttpRequest request,
+            ILogger log)
+        {
+            var data = await educationDataStore.GetAllEducationsAsync(log);
+            return new OkObjectResult(data);
+        }
+
+        [FunctionName("GetEducationById")]
+        public static async Task<IActionResult> GetEducation(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous, 
+                "get",
+                Route = "education/{id}"
+                )
+            ] HttpRequest request,
+            string id, 
+            ILogger log)
+        {
+            var idAsGuid = Guid.Parse(id);
+            var data = await educationDataStore.GetEducationAsync(idAsGuid, log);
+            return new OkObjectResult(data);
+        }
+    }
+}
---
+++ ng-resume-api/datastore/EducationDataStore.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Jpf.NgResume.Api.Models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Jpf.NgResume.Api.DataStore
+{
+    public class EducationDataStore
+    {
+        private static readonly string JSON_EMBEDDED_RESOURCE = "api.resources.education.json";
+
+        private bool isInitialized = false;
+        private IDictionary<Guid, Education> educationMap;
+
+        public EducationDataStore()
+        {
+            this.educationMap = new Dictionary<Guid, Education>();
+        }
+
+        public bool IsInitialized()
+        {
+            return this.isInitialized;
+        }
+
+        public async Task Initialize(ILogger log)
+        {
+            using (
+                Stream stream = Assembly
+                    .GetExecutingAssembly()
+                    .GetManifestResourceStream(JSON_EMBEDDED_RESOURCE)
+            )
+            using (
+                StreamReader reader = new StreamReader(stream)
+            )
+            {
+                var jsonFileContent = await reader.ReadToEndAsync();
+                var educationList = JsonConvert.DeserializeObject<IList<Education>>(jsonFileContent);
+
+                foreach (var item in educationList)
+                {
+                    educationMap.Add(item.Id, item);
+                }
+            }
+
+            this.isInitialized = true;
+        }
+
+        public async Task<IList<Education>> GetAllEducationsAsync(ILogger log)
+        {
+            if (!isInitialized) await Initialize(log);
+            return educationMap.Values.ToList<Education>();
+        }
+
+        public async Task<Education> GetEducationAsync(Guid id, ILogger log)
+        {
+            if (!isInitialized) await Initialize(log);
+            return educationMap[id];
+        }
+    }
+}
