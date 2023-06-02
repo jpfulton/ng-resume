45327ffdfdff9a66560fcea65c7d92ff88b13089
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-01 22:26:28 -0500

    Remove unused using statements.

--- ng-resume-api/EducationFunctions.cs
+++ ng-resume-api/EducationFunctions.cs
@@ -1,15 +1,12 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 using Jpf.NgResume.Api.DataStore;
-using Jpf.NgResume.Api.Models;
 
 namespace Jpf.NgResume.Api
 {
