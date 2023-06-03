cd669acf4adaed0beed8882226ec14f9776a8536
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-03 16:38:27 -0500

    Convert to pattern used by a newly generated app in standalone mode.

--- ng-resume-app/src/main.ts
+++ ng-resume-app/src/main.ts
@@ -1,31 +1,8 @@
 import { AppComponent } from './app/app.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { COOKIE_CONSENT_CONFIG } from './app/core/constants/cookieconsent-constants';
-import { NgcCookieConsentModule } from 'ngx-cookieconsent';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { HttpErrorInterceptor } from './app/core/interceptors/http-error.interceptor';
-import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { ApplicationinsightsAngularpluginErrorService } from '@microsoft/applicationinsights-angularplugin-js';
-import { ErrorHandler, importProvidersFrom } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
-import routeConfig from './app/routes';
+import { appConfig } from './app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 bootstrapApplication(
     AppComponent,
-    {
-        providers: [
-            importProvidersFrom(
-                BrowserModule,
-                RouterModule,
-                NgcCookieConsentModule.forRoot(COOKIE_CONSENT_CONFIG),
-                MatDialogModule
-            ),
-            provideRouter(routeConfig),
-            { provide: ErrorHandler, useClass: ApplicationinsightsAngularpluginErrorService },
-            { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
-            provideHttpClient(withInterceptorsFromDi()),
-            provideAnimations()
-            ]
-    }
+    appConfig
 ).catch(err => console.error(err));
