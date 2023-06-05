b96c14efe8c28a9eee29d56ae472c6aab45da1a0
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-05 11:27:20 -0500

    Remove unneeded module import.

--- ng-resume-app/src/app/app.config.ts
+++ ng-resume-app/src/app/app.config.ts
@@ -9,14 +9,13 @@ import { AppHttpInterceptor } from './core/interceptors/app-http.interceptor';
 import { HTTP_INTERCEPTORS, withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
 import { ApplicationinsightsAngularpluginErrorService } from '@microsoft/applicationinsights-angularplugin-js';
 import { ErrorHandler, importProvidersFrom } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import routeConfig from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         importProvidersFrom(
             BrowserModule,
-            RouterModule,
             NgcCookieConsentModule.forRoot(COOKIE_CONSENT_CONFIG),
             MatDialogModule
         ),
