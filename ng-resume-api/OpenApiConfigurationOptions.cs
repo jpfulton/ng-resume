7bf8159084ea17133e4faefdd8cd27ecf69d35c8
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-06 17:02:05 -0500

    Let's try this to solve the preview environment problem.

--- ng-resume-api/OpenApiConfigurationOptions.cs
+++ ng-resume-api/OpenApiConfigurationOptions.cs
@@ -8,6 +8,12 @@ namespace Jpf.NgResume.Api
 {
     public class OpenApiConfigurationOptions : IOpenApiConfigurationOptions
     {
+        private List<OpenApiServer> servers;
+
+        public OpenApiConfigurationOptions() {
+            servers = GetServerList();
+        }
+
         public OpenApiInfo Info { get; set; } =
           new OpenApiInfo
           {
@@ -25,13 +31,20 @@ namespace Jpf.NgResume.Api
               }
           };
 
-        public List<OpenApiServer> Servers { get; set; } = new();
+        public List<OpenApiServer> Servers { get { return this.servers; } set { } }
 
         public OpenApiVersionType OpenApiVersion { get; set; } = OpenApiVersionType.V3;
 
-        public bool IncludeRequestingHostName { get; set; } = true;
+        public bool IncludeRequestingHostName { get; set; } = false;
         public bool ForceHttp { get; set; } = false;
         public bool ForceHttps { get; set; } = false;
         public List<IDocumentFilter> DocumentFilters { get; set; } = new();
+
+        private static List<OpenApiServer> GetServerList() {
+            var list = new List<OpenApiServer>();
+            list.Add(new OpenApiServer() { Url = "/api/" });
+
+            return list;
+        }
     }
 }
