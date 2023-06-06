51f29895f42498923fd82883e8af1eff57d24bc3
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-06 15:44:28 -0500

    Add OpenAPI and Swagger attributes with additional error handling.

--- ng-resume-api/WorkHistoryFunctions.cs
+++ ng-resume-api/WorkHistoryFunctions.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
 using Jpf.NgResume.Api.DataStore;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using System.Net;
+using System.Collections.Generic;
+using Jpf.NgResume.Api.Models;
+using Microsoft.OpenApi.Models;
 
 namespace Jpf.NgResume.Api
 {
@@ -24,6 +29,13 @@ namespace Jpf.NgResume.Api
         }
 
         [FunctionName("WorkHistoryGetAll")]
+        [OpenApiOperation(operationId: "WorkHistoryGetAll", tags: new[] { "workhistory" })]
+        [OpenApiResponseWithBody(
+            statusCode: HttpStatusCode.OK,
+            contentType: "application/json; charset=utf-8",
+            bodyType: typeof(IList<WorkHistory>),
+            Description = "An array of all work history items."
+        )]
         public static async Task<IActionResult> GetAllWorkHistory(
             [HttpTrigger(
                 AuthorizationLevel.Anonymous, 
@@ -38,6 +50,18 @@ namespace Jpf.NgResume.Api
         }
 
         [FunctionName("WorkHistoryGetById")]
+        [OpenApiOperation(operationId: "WorkHistoryGetById", tags: new[] { "workhistory" })]
+        [OpenApiParameter(
+            name: "id", 
+            Required = true, 
+            In = ParameterLocation.Path, 
+            Type = typeof(Guid))]
+        [OpenApiResponseWithBody(
+            statusCode: HttpStatusCode.OK,
+            contentType: "application/json; charset=utf-8",
+            bodyType: typeof(WorkHistory),
+            Description = "A work history item by its id property."
+        )]
         public static async Task<IActionResult> GetWorkHistory(
             [HttpTrigger(
                 AuthorizationLevel.Anonymous, 
@@ -48,7 +72,17 @@ namespace Jpf.NgResume.Api
             string id, 
             ILogger log)
         {
-            var idAsGuid = Guid.Parse(id);
+            if ( string.IsNullOrEmpty(id))
+                throw new ArgumentNullException("id", "Id is a required parameter.");
+
+            Guid idAsGuid;
+            try {
+                idAsGuid = Guid.Parse(id);
+            }
+            catch (FormatException fe) {
+                throw new ArgumentException("Id parameter is not in the form of a guid.", "id", fe);
+            }
+            
             var data = await dataStore.GetWorkHistoryAsync(idAsGuid);
             return new OkObjectResult(data);
         }
