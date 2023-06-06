71417d20a9cec338403cfc82932ef40a16d9a2dc
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-06 16:08:39 -0500

    Adjust settings for preview and production environments.

--- ng-resume-api/OpenApiConfigurationOptions.cs
+++ ng-resume-api/OpenApiConfigurationOptions.cs
@@ -30,7 +30,7 @@ namespace Jpf.NgResume.Api
         public OpenApiVersionType OpenApiVersion { get; set; } = OpenApiVersionType.V2;
 
         public bool IncludeRequestingHostName { get; set; } = true;
-        public bool ForceHttp { get; set; } = true;
+        public bool ForceHttp { get; set; } = false;
         public bool ForceHttps { get; set; } = false;
         public List<IDocumentFilter> DocumentFilters { get; set; } = new();
     }
