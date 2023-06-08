75c19a6794da0dd211dac82b20f7b22e7a037cf5
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-08 18:03:03 -0500

    Clean up header and move items to menu.

--- ng-resume-app/src/app/core/components/header/header.component.ts
+++ ng-resume-app/src/app/core/components/header/header.component.ts
@@ -1,16 +1,15 @@
-import { AfterViewInit, Component, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { Subscription } from 'rxjs';
+import { MatDividerModule } from '@angular/material/divider';
+ 
+import { FocusableDirective } from '../../directives/focusable.directive';
 
 @Component({
   selector: 'app-header',
@@ -25,29 +24,10 @@ import { Subscription } from 'rxjs';
     MatSlideToggleModule,
     MatIconModule,
     MatMenuModule,
-    MatTooltipModule
+    MatDividerModule,
+    FocusableDirective
   ]
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent {
   @ViewChild("darkModeToggle") darkModeToggle!: MatSlideToggle;
-  isHandset = false;
-
-  private breakpointSubscription: Subscription | undefined;
-
-  constructor(private breakpointObserver: BreakpointObserver) { }
-
-  ngOnInit(): void {
-    this.breakpointSubscription = this.breakpointObserver.observe(Breakpoints.Handset).subscribe(result => {
-      if (result.matches) {
-        this.isHandset = true;
-      }
-      else {
-        this.isHandset = false;
-      }
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.breakpointSubscription?.unsubscribe();
-  }
 }
