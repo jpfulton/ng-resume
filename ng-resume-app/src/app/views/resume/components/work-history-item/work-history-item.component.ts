461829d76956549663ce3a2f96b3487436ce21b8
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-07 23:06:27 -0500

    Correct lint warning by removing unused import.

--- ng-resume-app/src/app/views/resume/components/work-history-item/work-history-item.component.ts
+++ ng-resume-app/src/app/views/resume/components/work-history-item/work-history-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
