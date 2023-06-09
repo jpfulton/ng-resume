f0f5bdac771d52969d844c339c2c812c4de8a7a9
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-09 16:33:52 -0500

    Updated to use node_modules version of fern bin.

--- ng-resume-api-fern/scripts/init-fern.sh
+++ ng-resume-api-fern/scripts/init-fern.sh
@@ -4,6 +4,8 @@ WORKING_DIR=$( pwd; )
 LOCAL_API_URL="http://localhost:7071/api/openapi/v3.yml"
 TARGET_API_YML_FILE="live-openapi.yml"
 
+FERN_BIN="./node_modules/.bin/fern"
+
 FERN_DIR="./fern"
 FERN_GENERATORS_DIR="./fern/api"
 REPLACEMENT_FERN_CONFIG="./resources/fern.config.json"
@@ -21,15 +23,15 @@ curl -s -o $TARGET_API_YML_FILE $LOCAL_API_URL
 echo "---"
 
 echo "Initializing fern from downloaded OpenAPI document"
-echo "Using fern version: $( fern --version; )"
-fern init --openapi $TARGET_API_YML_FILE
+echo "Using fern version: $( $FERN_BIN --version; )"
+$FERN_BIN init --openapi $TARGET_API_YML_FILE
 
 echo "Replacing generated fern artifacts with versioned resource files..."
 cp -f -p $REPLACEMENT_FERN_CONFIG $FERN_DIR
 cp -f -p $REPLACMENT_GENERATORS_YML $FERN_GENERATORS_DIR
 
 echo "Running fern generate..."
-fern generate --log-level="debug"
+$FERN_BIN generate --log-level="debug"
 
 echo "---"
 echo "Removing temporary copy of OpenAPI document..."
