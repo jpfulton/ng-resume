6f46981a03dc4d77ab17b4a14520dfbf27ad46be
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-10 16:57:44 -0500

    Move logic from AppHttpInterceptor to LoadingService.

--- ng-resume-app/src/app/core/interceptors/app-http.interceptor.ts
+++ ng-resume-app/src/app/core/interceptors/app-http.interceptor.ts
@@ -18,16 +18,13 @@ import { LoadingService } from '../services/loading.service';
 @Injectable()
 export class AppHttpInterceptor implements HttpInterceptor {
 
-  private totalRequests = 0;
-
   constructor(
     private loadingService: LoadingService
   ) 
   {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    this.totalRequests++;
-    this.loadingService.setLoading(true);
+    this.loadingService.incrementTotalRequests();
 
     if (request.method === "GET") {
       return next.handle(request).pipe(
@@ -43,10 +40,6 @@ export class AppHttpInterceptor implements HttpInterceptor {
   }
 
   private manageTotalRequests() {
-    this.totalRequests--;
-
-    if (this.totalRequests == 0) {
-      this.loadingService.setLoading(false);
-    }
+    this.loadingService.decrementTotalRequests();
   }
 }
