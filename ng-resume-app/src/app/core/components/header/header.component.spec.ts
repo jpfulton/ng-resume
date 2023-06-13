8fcb3cfed3bac9b7a614075af843316c30209eab
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-12 19:24:54 -0500

    Fix unit tests.

--- ng-resume-app/src/app/core/components/header/header.component.spec.ts
+++ ng-resume-app/src/app/core/components/header/header.component.spec.ts
@@ -3,6 +3,8 @@ import { RouterModule, provideRouter } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
 import routeConfig from 'src/app/app.routes';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalModule, MsalService } from '@azure/msal-angular';
+import { MSALInstanceFactory, MSALGuardConfigFactory } from 'src/app/app.config';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -12,10 +14,14 @@ describe('HeaderComponent', () => {
     TestBed.configureTestingModule({
       imports: [
         RouterModule,
-        HeaderComponent
+        HeaderComponent,
+        MsalModule
       ],
       providers: [
         provideRouter(routeConfig),
+        MsalService,
+        { provide: MSAL_INSTANCE, useFactory: MSALInstanceFactory },
+        { provide: MSAL_GUARD_CONFIG, useFactory: MSALGuardConfigFactory },
       ]
     });
     fixture = TestBed.createComponent(HeaderComponent);
--- ng-resume-app/src/app/views/claims/claims-view.component.spec.ts
+++ ng-resume-app/src/app/views/claims/claims-view.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClaimsViewComponent } from './claims-view.component';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalModule, MsalService } from '@azure/msal-angular';
+import { MSALInstanceFactory, MSALGuardConfigFactory } from 'src/app/app.config';
 
 describe('ClaimsViewComponent', () => {
   let component: ClaimsViewComponent;
@@ -8,7 +10,15 @@ describe('ClaimsViewComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ClaimsViewComponent]
+      imports: [
+        ClaimsViewComponent,
+        MsalModule
+      ],
+      providers: [
+        MsalService,
+        { provide: MSAL_INSTANCE, useFactory: MSALInstanceFactory },
+        { provide: MSAL_GUARD_CONFIG, useFactory: MSALGuardConfigFactory },
+      ]
     });
     fixture = TestBed.createComponent(ClaimsViewComponent);
     component = fixture.componentInstance;
