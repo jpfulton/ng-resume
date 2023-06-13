18d89d4467edfaaadccbcb7b7c77e48458b14865
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-13 15:13:39 -0500

    Update unit tests.

--- ng-resume-app/src/app/core/services/auth.service.spec.ts
+++ ng-resume-app/src/app/core/services/auth.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalModule, MsalService } from '@azure/msal-angular';
 
 import { AuthService } from './auth.service';
+import { MSALGuardConfigFactory, MSALInstanceFactory } from 'src/app/app.config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        MsalModule
+      ],
+      providers: [
+        MsalService,
+        { provide: MSAL_INSTANCE, useFactory: MSALInstanceFactory },
+        { provide: MSAL_GUARD_CONFIG, useFactory: MSALGuardConfigFactory },
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
