80283f85951ee831c23d5292e92d0f7e3bc5ce56
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-19 14:32:36 -0500

    Add better logging.

--- ng-resume-api/CustomJwtBearerHandler.cs
+++ ng-resume-api/CustomJwtBearerHandler.cs
@@ -31,14 +31,16 @@ namespace Jpf.NgResume.Api
                 // if the standard token is missing, we are working with the client
                 // which uses a sepearate header to bear its Jwt tokens.
                 var token = Request.Headers[CustomJwtBearerConstants.HeaderName];
+                Logger.LogInformation($"Bearer token: {token}");
 
-                // place that token into the "standar" header for processing inside
+                // place that token into the "standard" header for processing inside
                 // the base JwtBearerHandler implementation which offers no configurable
                 // mechanism to specific the header to use at time of writing (6/19/2023)
                 Request.Headers.Add("Authorization", token);
             }
             else {
                 Logger.LogInformation("Request incoming from Azure or function host.");
+                Logger.LogInformation($"Bearer token: {standardAuthToken}");
             }
 
             // call base implementation to address authentication on token
