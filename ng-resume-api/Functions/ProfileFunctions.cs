88cfcbce47d14a1b8c99adddd8d71aba704292c5
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-20 09:47:14 -0500

    Create profile endpoint.

---
+++ ng-resume-api/Functions/ProfileFunctions.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using System.Threading.Tasks;
+using Jpf.NgResume.Api.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.OpenApi.Models;
+
+namespace Jpf.NgResume.Api.Functions
+{
+
+    public class ProfileFunctions
+    {
+        private readonly GraphServiceClient graphServiceClient;
+
+        public ProfileFunctions(GraphServiceClient graphServiceClient)
+        {
+            this.graphServiceClient = graphServiceClient;
+        }
+
+        [FunctionName("ProfileGet")]
+        [OpenApiOperation(operationId: "Get", tags: new[] { "profile" })]
+        [OpenApiSecurity(
+            "Bearer", 
+            SecuritySchemeType.Http, 
+            Scheme = OpenApiSecuritySchemeType.Bearer, 
+            BearerFormat = "JWT",
+            In = OpenApiSecurityLocationType.Header)]
+        [OpenApiRequestBody(
+            contentType: "application/json; charset=utf-8",
+            bodyType: typeof(User)
+        )]
+        [OpenApiResponseWithBody(
+            statusCode: HttpStatusCode.OK,
+            contentType:  "application/json; charset=utf-8",
+            bodyType: typeof(User),
+            Description = "A user representing the current user's profile."
+        )]
+        [OpenApiResponseWithBody(
+            statusCode: HttpStatusCode.Unauthorized,
+            contentType:  "application/problem+json; charset=utf-8",
+            bodyType: typeof(CustomProblemDetails),
+            Description = "Problem details of an unauthorized access result."
+        )]
+        public async Task<IActionResult> GetAsync(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "get",
+                Route = "profile"
+                )
+            ]
+            HttpRequest req,
+            ILogger log)
+        {
+            var me = await graphServiceClient.Me.GetAsync();
+            return new OkObjectResult(me);
+        }
+    }
+}
