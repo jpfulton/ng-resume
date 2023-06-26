610305c955a312b25751ff26d583c3b278e6a0e3
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-06-25 19:37:45 -0500

    Progress on users screen.

--- ng-resume-app/src/app/views/users/users-view.component.ts
+++ ng-resume-app/src/app/views/users/users-view.component.ts
@@ -5,25 +5,38 @@ import { User } from '@jpfulton/ng-resume-api-browser-sdk/api';
 import { Subscription } from 'rxjs';
 import { MatTableModule } from '@angular/material/table';
 import { PlatformService } from 'src/app/core/services/platform.service';
+import { MatIconModule } from '@angular/material/icon';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-users-view',
   standalone: true,
   imports: [
     CommonModule,
-    MatTableModule
+    MatTableModule,
+    MatIconModule
   ],
   templateUrl: './users-view.component.html',
-  styleUrls: ['./users-view.component.scss']
+  styleUrls: ['./users-view.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+
 })
 export class UsersViewComponent implements OnInit, OnDestroy {
   userList: User[] = [];
+  expandedUser: User | null = null;
   displayedColumns: string[] = [
     "id",
     "displayName",
     "givenName",
     "surname"
   ];
+  columnsToDisplayWithExpand = [...this.displayedColumns, 'expand'];
 
   private usersSubscription: Subscription | null = null;
 
