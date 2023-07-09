3f6213f8e8c76d3652f30d10769e46e096359f1f
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-07-08 22:39:02 -0500

    Remove unused import.

--- ng-resume-api/Auth/AuthenticationHelpers.cs
+++ ng-resume-api/Auth/AuthenticationHelpers.cs
@@ -1,6 +1,5 @@
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
