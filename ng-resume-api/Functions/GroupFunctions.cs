d763bb5701451a5f618e216584731ef3d141f0d0
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-03 22:14:20 -0500

    Add group membership methods.

--- ng-resume-api/Functions/GroupFunctions.cs
+++ ng-resume-api/Functions/GroupFunctions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -5,9 +6,9 @@ using System.Threading.Tasks;
 using Jpf.NgResume.Api.Auth;
 using Jpf.NgResume.Api.MicrosoftGraph;
 using Jpf.NgResume.Api.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker.Extensions.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Graph;
@@ -98,6 +99,109 @@ namespace Jpf.NgResume.Api.Functions
             var response = request.CreateResponse();
             await response.WriteAsJsonAsync(appGroups);
 
+            return response;
+        }
+
+        [Function("GroupsAddUserToGroup")]
+        [OpenApiOperation(operationId: "AddUser", tags: new[] { "groups" })]
+        [OpenApiSecurity(
+            "Bearer",
+            SecuritySchemeType.Http,
+            Scheme = OpenApiSecuritySchemeType.Bearer,
+            BearerFormat = "JWT",
+            In = OpenApiSecurityLocationType.Header)]
+        [OpenApiRequestBody(
+            contentType: "application/problem+json; charset=utf-8",
+            bodyType: typeof(Models.User),
+            Description = "User to be added to the group.",
+            Required = true
+        )]
+        [OpenApiResponseWithoutBody(
+            statusCode: HttpStatusCode.NoContent,
+            Description = "User has been added to the group."
+        )]
+        [OpenApiResponseWithBody(
+            statusCode: HttpStatusCode.Unauthorized,
+            contentType: "application/problem+json; charset=utf-8",
+            bodyType: typeof(CustomProblemDetails),
+            Description = "Problem details of an unauthorized access result."
+        )]
+        public async Task<HttpResponseData> AddUserToGroupAsync(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "post",
+                Route = "groups/{groupId}/users"
+                )
+            ]
+            HttpRequestData request,
+            [FromBody] Models.User user,
+            string groupId,
+            FunctionContext functionContext)
+        {
+            var log = functionContext.GetLogger<GroupFunctions>();
+
+            var (authorized, authorizationResponse) =
+                await request.AuthenticateThenAuthorizeWithGroup(
+                    functionContext,
+                    graphClient,
+                    log,
+                    "SiteOwners");
+            if (!authorized) return authorizationResponse;
+
+            var directoryObject = new DirectoryObject
+            {
+                Id = user.Id,
+            };
+            await graphClient.Groups[groupId].Members.References.Request().AddAsync(directoryObject);
+
+            var response = request.CreateResponse(HttpStatusCode.NoContent);
+            return response;
+        }
+
+        [Function("GroupsRemoveUserFromGroup")]
+        [OpenApiOperation(operationId: "RemoveUser", tags: new[] { "groups" })]
+        [OpenApiSecurity(
+            "Bearer",
+            SecuritySchemeType.Http,
+            Scheme = OpenApiSecuritySchemeType.Bearer,
+            BearerFormat = "JWT",
+            In = OpenApiSecurityLocationType.Header
+        )]
+        [OpenApiResponseWithoutBody(
+            statusCode: HttpStatusCode.NoContent,
+            Description = "User has been removed from the group."
+        )]
+        [OpenApiResponseWithBody(
+            statusCode: HttpStatusCode.Unauthorized,
+            contentType: "application/problem+json; charset=utf-8",
+            bodyType: typeof(CustomProblemDetails),
+            Description = "Problem details of an unauthorized access result."
+        )]
+        public async Task<HttpResponseData> RemoveUserFromGroupAsync(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "delete",
+                Route = "groups/{groupId}/users/{userId}"
+                )
+            ]
+            HttpRequestData request,
+            string groupId,
+            string userId,
+            FunctionContext functionContext)
+        {
+            var log = functionContext.GetLogger<GroupFunctions>();
+
+            var (authorized, authorizationResponse) =
+                await request.AuthenticateThenAuthorizeWithGroup(
+                    functionContext,
+                    graphClient,
+                    log,
+                    "SiteOwners");
+            if (!authorized) return authorizationResponse;
+
+            await graphClient.Groups[groupId].Members[userId].Reference.Request().DeleteAsync();
+
+            var response = request.CreateResponse(HttpStatusCode.NoContent);
             return response;
         }
     }
